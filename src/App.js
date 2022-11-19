6f7d727c89420d576a052497c0e1e9841f05a61c
Simon Miner <simon.miner@gmail.com>
2022-11-18 22:20:09 -0500

    Integrate React clock

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import Clock from 'react-clock';
 import './App.css';
+import 'react-clock/dist/Clock.css';
 
 function App() {
+  const [now, setValue] = useState(new Date());
+
+  useEffect(() => {
+    const interval = setInterval(() => setValue(new Date()), 1000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Current Time</h1>
+      <Clock 
+        value={now}
+        size={500}
+        renderNumbers={true}
+      />
     </div>
   );
 }
