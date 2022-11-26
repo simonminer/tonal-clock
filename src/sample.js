c1a2c50f2032df07e452d1eff45954fcf5fecb37
Simon Miner <simon.miner@gmail.com>
2022-11-25 21:12:38 -0500

    Add ability to shift pitch on sample playback

--- src/sample.js
+++ src/sample.js
@@ -4,34 +4,66 @@ export default class Sample {
      * Path to file containing the audio sample.
      */
     path;
-    audioContext;
-    data;
 
-    constructor(path) {
+    /**
+     * AudioContext to manage audio buffer manipulation and playback.
+     */
+    audioContext;
+
+    /**
+     * Raw binary data from the sample audio file.
+     */
+    buffer;
+
+    constructor(path, audioContext) {
         this.path = path;
+        this.audioContext = audioContext;
     }
 
+    /**
+     * Fetches a buffer of data for the specified sample file.
+     * @param {String} path Path/UL to the sample file to be retrieved
+     * @returns ArrayBuffer
+     */
     get(path) {
-        if (this.data) {
-            return this.data;
+        if (this.buffer) {
+            return this.buffer;
         }
         else {
             return fetch(this.path)
                 .then(response => response.arrayBuffer())
                 .then(arrayBuffer => {
-                    this.data = this.audioContext.decodeAudioData(arrayBuffer);
-                    return this.data;
+                    this.buffer = this.audioContext.decodeAudioData(arrayBuffer);
+                    return this.buffer;
                 });
         }
     }
 
-    play() {
+    /**
+     * Determine the playback rate for the sample based on the specified
+     * pitch shift (semitone count).
+     * @param {int} pitchShift 
+     * @returns float
+     */
+    computePlaybackRate(pitchShift) {
+        const playbackRate = Math.pow(2, pitchShift / 12);
+        return playbackRate;
+    }
+
+    /**
+     * Plays the sample with the specified pitch shift (if any).
+     * @param {int} pitchShift Number of semitones to shift the sample's pitch up or down
+     */
+    play(pitchShift = 0) {
         this.get(this.path)
             .then(audioBuffer => {
                 let sourceNode = this.audioContext.createBufferSource();
                 sourceNode.buffer = audioBuffer;
+                if (pitchShift) {
+                    sourceNode.playbackRate.value = this.computePlaybackRate(pitchShift);
+                }
                 sourceNode.connect(this.audioContext.destination);
-                //sourceNode.start(0,0,0.25);
+                sourceNode.start(0,0,0.25);
             });
     }
 }
--- src/sample.test.js
+++ src/sample.test.js
@@ -7,13 +7,23 @@ beforeEach(() => {
 var sample;
 var samplePath = '';
 beforeEach(() => {
-    sample = new Sample(samplePath);
+    sample = new Sample(samplePath, window.AudioContext);
 });
 
 describe('Sample tests', () => {
-    it('creates a new sample objectt', async () => {
+    it('creates a new sample objectt', () => {
         expect(sample instanceof Sample).toBe(true);
         expect(sample.path).toBe(samplePath);
-        //expect(sample.data).toBeDefined();
+        expect(sample.AudioContext).toBe(window.audioContext);
+    });
+
+    it('computes playback rate', () => {
+        for (let pitchShift = -3; pitchShift <= 3; pitchShift++) {   
+            if (pitchShift === 0) {
+                continue;
+            }
+            const playbackRate = Math.pow(2, pitchShift / 12);
+            expect(sample.computePlaybackRate(pitchShift)).toBe(playbackRate);
+        }
     });
 });
