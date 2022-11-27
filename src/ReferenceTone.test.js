bfe2b6df6a80126e5f0dbcebceea87ce60efc9f4
Simon Miner <simon.miner@gmail.com>
2022-11-27 16:43:33 -0500

    Use Howler to play reference tone

---
+++ src/ReferenceTone.test.js
@@ -0,0 +1,10 @@
+import { render, screen } from '@testing-library/react';
+import ReferenceTone from './ReferenceTone.js';
+
+jest.mock('howler');
+
+test('renders reference tone', () => {
+  const {container} = render(<ReferenceTone />);
+  const referenceToneElement = screen.getByText('Reference: C2');
+  expect(referenceToneElement).toBeInTheDocument();
+});
